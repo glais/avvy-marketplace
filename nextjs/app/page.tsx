cfc2e0f6abe4984b1a10e90834c9ea691723dc64
Connor Bode <connor.bode@gmail.com>
2023-06-09 13:08:08 -0400

    pull data from django backend

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,19 +1,27 @@
-import { Domain, columns } from "./components/columns"
-import { DataTable } from "./components/data-table"
+import Layout from 'app/layout'
+import { Domain, columns } from "app/components/columns"
+import { DataTable } from "app/components/data-table"
+import { useRouter } from 'next/router'
+
+import { useEffect, useState } from 'react'
  
 
-async function getData(): Promise<Domain[]> {
-  const response = await fetch('http://127.0.0.1:5000/api/domains');
-  return response.json();
+async function getData(slug): Domain[] {
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/collections/${slug}/`
+  const response = await fetch(url, { cache: 'no-store' });
+  const body = await response.json()
+  console.log(body)
+  return body.data;
 }
  
-export default async function Home() {
-  const data = await getData()
-  console.log(data)
+export default async function Page() {
+  const data = await getData('first-names')
  
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
+    <Layout>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </Layout>
   )
 }
